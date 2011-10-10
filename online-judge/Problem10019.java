d1b9bcb22dbb83b8b783d901cb93e52842c7e973
Zach Langley <zbl9222@cs.rit.edu>
2011-10-10 13:37:50 -0400

    adding solution to Funny Encryption Method

---
+++ online-judge/Problem10019.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// Funny Encryption Method.
+public class Problem10019 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+		for (int i = 0; i < n; i++) {
+			String s = scan.next();
+			System.out.println(countOnesInBinary(Integer.parseInt(s)) + " " +
+					countOnesInBinary(Integer.parseInt(s, 16)));
+		}
+	}
+
+	public static int countOnesInBinary(int a) {
+		int count = 0;
+		for (int i = 1; i <= a; i <<= 1)
+			if ((a & i) != 0)
+				count++;
+
+		return count;
+	}
+}
