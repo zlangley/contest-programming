d8f365a7e90519760f42303c3b4cca05668b9fa5
Zach Langley <zbl9222@cs.rit.edu>
2011-10-14 21:57:32 -0400

    adding solutions to 10271 - Chopsticks

---
+++ online-judge/10271.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+// Chopsticks.
+class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int probs = scan.nextInt();
+
+		for (int i = 0; i < probs; i++) {
+			int k = scan.nextInt();
+			int n = scan.nextInt();
+
+			int[] sticks = new int[n + 1];
+			for (int j = 0; j < n; j++)
+				sticks[sticks.length - j - 1] = scan.nextInt();
+
+			int[][] m = new int[n + 1][k + 9];
+			for (int c = 1; c < m[0].length; c++)
+				for (int r = 0; r < m.length; r++)
+					m[r][c] = Integer.MAX_VALUE;
+
+			for (int c = 1; c < m[0].length; c++) {
+				for (int r = 3*c; r < m.length; r++) {
+					int sqrtBadness = sticks[r - 1] - sticks[r];
+					m[r][c] = Math.min(m[r - 1][c], m[r - 2][c - 1] + sqrtBadness*sqrtBadness);
+				}
+			}
+
+			System.out.println(m[n][k + 8]);
+		}
+	}
+}
