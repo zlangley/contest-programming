ae23b558ad32584edc177a8081a9af5ebaa57ae1
Zach Langley <zbl9222@cs.rit.edu>
2011-10-28 21:34:21 -0400

    adding solution to The Archeologists' Dilemma

---
+++ online-judge/00701.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		double log10 = Math.log(10)/Math.log(2);
+
+		while (scan.hasNext()) {
+			String next = scan.next();
+			int n = Integer.parseInt(next);
+			int m = next.length();
+
+			int k;
+			do {
+				m++;
+				k = (int)Math.ceil(lg(n) + m * lg(10));
+			} while (k > m * lg(10) + lg(n + 1));
+
+			System.out.println(k);
+		}
+	}
+
+	public static double lg(int a) {
+		return Math.log(a) / Math.log(2);
+	}
+}
