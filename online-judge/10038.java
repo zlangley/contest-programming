9b26774be3a024b47d63971b8f9e69d1a9be62f4
Zach Langley <zbl9222@cs.rit.edu>
2011-10-28 21:35:55 -0400

    adding solution to Jolly Jumpers

---
+++ online-judge/10038.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		while (scan.hasNextInt()) {
+			int n = scan.nextInt();
+			boolean[] used = new boolean[n];
+
+			int last = scan.nextInt();
+			for (int i = 1; i < n; i++) {
+				int next = scan.nextInt();
+				try {
+					used[Math.abs(last - next)] = true;
+				} catch (IndexOutOfBoundsException e) { }
+				last = next;
+			}
+
+			boolean jolly = true;
+			for (int i = 1; i < n; i++) {
+				if (!used[i]) {
+					System.out.println("Not jolly");
+					jolly = false;
+					break;
+				}
+			}
+			if (jolly)
+				System.out.println("Jolly");
+		}
+	}
+}
