a3d1c86c47b745ab03929eb7a07fc9d8903362c4
Zach Langley <zbl9222@cs.rit.edu>
2012-08-23 04:34:09 -0400

    adding solutions to a few more problems

---
+++ acm-icpc/gnyr2006/A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+		for (int i = 0; i < n; i++) {
+			int amount = scan.nextInt();
+			System.out.printf("%d %d QUARTER(S), %d DIME(S), %d NICKEL(S), %d PENNY(S)\n",
+					i + 1, amount/25, amount%25/10, amount%25%10/5, amount%5);
+		}
+	}
+}
